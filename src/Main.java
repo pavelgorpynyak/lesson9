5ad9cc94f2f50b3eab8865621033f357d27d1b43
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-06-29 18:42:48 +0300

    lesson9 is finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person sarah = new Person("Sarah", 30);
+        System.out.println("sarah.name = " + sarah.getName());
+        System.out.println("sarah.age = " + sarah.getAge());
+        sarah.setAge(31);
+        System.out.println("sarah.getAge() = " + sarah.getAge());
+        Person john = new Person("John", 13);
+
     }
+
 }
